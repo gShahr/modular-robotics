1c219394b23840f3e4d0dc3d4cf6907bd8dda09d
Gabriel Shahrouzi <gshahrouzi@gmail.com>
2024-07-23 14:40:37 -0400

    Add setupfinal using json

--- LatticeSetup.cpp
+++ LatticeSetup.cpp
@@ -21,7 +21,28 @@ namespace LatticeSetup {
         Lattice::BuildMovableModules();
     }
 
+    CoordTensor<bool> setupFinalFromJson(const std::string& filename) {
+        std::ifstream file(filename);
+        if (!file) {
+            std::cerr << "Unable to open file " << filename << std::endl;
+            return;
+        }
+        nlohmann::json j;
+        file >> j;
+        CoordTensor<bool> desiredState(Lattice::Order(), Lattice::AxisSize(), false);
+        for (const auto& module : j["modules"]) {
+            std::vector<int> position = module["position"];
+            std::transform(position.begin(), position.end(), position.begin(),
+                        [](int coord) { return coord; });
+            std::valarray<int> coords(position.data(), position.size());
+            desiredState[coords] = true;
+        }
+        return desiredState;
+
+    }
+
     void setupInitial(const std::string& filename) {
+        Lattice::InitLattice(2, 9);
         std::vector<std::vector<char>> image;
         int x = 0;
         int y = 0;
