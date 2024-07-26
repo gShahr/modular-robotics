d8f4e15277d88d0e2b7dcbdbda3820d6fe20fb0d
Andrew Clements <andrew.clements.504@gmail.com>
2024-07-26 13:58:45 -0400

    Fixed Isometry and ITransformable

--- Isometry.cpp
+++ Isometry.cpp
@@ -6,37 +6,16 @@ std::vector<ITransformable*> Isometry::transformsToFree;
 std::vector<ITransformable*> Isometry::GenerateTransforms(ITransformable* initial) {
     // Set up working vector
     std::vector<ITransformable*> transforms = {initial};
-    // Reflection
-    auto reflected = initial->MakeCopy();
-    reflected->Reflect(0);
-    transforms.push_back(reflected);
-    transformsToFree.push_back(reflected);
     // Rotations
-    for (int i = 0; i < initial->order; i++) {
+    for (int i = 0; i < initial->order -1; i++) {
+        auto forms = transforms;
         for (int j = i + 1; j < initial->order; j++) {
-            auto forms = transforms;
             for (auto form : forms) {
                 auto rotated = form->MakeCopy();
                 rotated->Rotate(i, j);
-                // Reflections of Rotations
-                for (int k = 0; k < initial->order; k++) {
-                    auto reflectedRotate = rotated->MakeCopy();
-                    reflectedRotate->Reflect(k);
-                    transforms.push_back(reflectedRotate);
-                    transformsToFree.push_back(reflectedRotate);
-                }
                 transforms.push_back(rotated);
                 transformsToFree.push_back(rotated);
             }
-        }
-    }
-    /*// Rotations
-    for (int i = 0; i < initial->order; i++) {
-        for (int j = i + 1; j < initial->order; j++) {
-            auto rotated = initial->MakeCopy();
-            rotated->Rotate(i, j);
-            transforms.push_back(rotated);
-            transformsToFree.push_back(rotated);
         }
     }
     // Reflections
@@ -48,7 +27,7 @@ std::vector<ITransformable*> Isometry::GenerateTransforms(ITransformable* initia
             transforms.push_back(reflected);
             transformsToFree.push_back(reflected);
         }
-    }*/
+    }
     // Done
     return transforms;
 }
