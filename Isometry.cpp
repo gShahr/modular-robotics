dd792ad77109210fa531fc0f492ccf4df6d09732
Andrew Clements <andrew.clements.504@gmail.com>
2024-07-26 14:20:55 -0400

    Vital formatting change

--- Isometry.cpp
+++ Isometry.cpp
@@ -7,7 +7,7 @@ std::vector<ITransformable*> Isometry::GenerateTransforms(ITransformable* initia
     // Set up working vector
     std::vector<ITransformable*> transforms = {initial};
     // Rotations
-    for (int i = 0; i < initial->order -1; i++) {
+    for (int i = 0; i < initial->order - 1; i++) {
         auto forms = transforms;
         for (int j = i + 1; j < initial->order; j++) {
             for (auto form : forms) {
