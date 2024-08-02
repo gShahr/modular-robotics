f1fba101ac6f253c6111f01dd24a152e8ee059e0
Gabriel Shahrouzi <gshahrouzi@gmail.com>
2024-08-01 22:49:25 -0400

    Fixed rhomdod creation in 3D by making cubes and adding pyramid faces to each face of the cube
    - Implemented drawRhombicDodecahedrons method to create cubes and add pyramids to each face.
    - Noted that rendering is slow due to the large number of shapes drawn each time.
    - Acknowledged that the current implementation allows intersections between shapes, which should be avoided.

--- 3DTiler/src/3dScreen.js
+++ 3DTiler/src/3dScreen.js
@@ -161,23 +161,57 @@ class threeDScreen {
         }
     }
 
-    drawRhombicDodecahedron(sketch, size) {
+    drawRhombicDodecahedron(sketch) {
         // coords from https://stackoverflow.com/questions/29314787/three-js-trying-to-render-a-custom-mesh-rhombic-dodecahedron
+        // const vertices = [
+        //     [-1, 1, -1], // A       (0)
+        //     [1, 1, -1],  // B       (1)
+        //     [1, 1, 1],   // C       (2)
+        //     [-1, 1, 1],  // D       (3)
+        //     [-1, -1, -1],// E       (4)
+        //     [1, -1, -1], // F       (5)
+        //     [1, -1, 1],  // G       (6)
+        //     [-1, -1, 1], // H       (7)
+        //     [-2, 0, 0],  // left    (8)
+        //     [2, 0, 0],   // right   (9)
+        //     [0, 2, 0],   // top     (10)
+        //     [0, -2, 0],  // bottom  (11)
+        //     [0, 0, 2],   // front   (12)
+        //     [0, 0, -2]   // back    (13)
+        // ];
+        
+        // const faces = [
+        //     [12, 2, 10, 3],  // (front, C, top, D)
+        //     [12, 6, 9, 2],   // (front, G, right, C)
+        //     [12, 7, 11, 6],  // (front, H, bottom, G)
+        //     [12, 3, 8, 7],   // (front, D, left, H)
+        //     [13, 5, 11, 4],  // (back, F, bottom, E)
+        //     [13, 4, 8, 0],   // (back, E, left, A)
+        //     [13, 0, 10, 1],  // (back, A, top, B)
+        //     [13, 1, 9, 5],   // (back, B, right, F)
+        //     [8, 3, 10, 0],   // (left, D, top, A)
+        //     [8, 4, 11, 7],   // (left, E, bottom, H)
+        //     [9, 1, 10, 2],   // (right, B, top, C)
+        //     [9, 6, 11, 5]    // (right, G, bottom, F)
+        // ];
+
+        const cos45 = Math.sqrt(2) / 2;
+        const sin45 = Math.sqrt(2) / 2;    
         const vertices = [
-            [-1, 1, -1], // A       (0)
-            [1, 1, -1],  // B       (1)
-            [1, 1, 1],   // C       (2)
-            [-1, 1, 1],  // D       (3)
-            [-1, -1, -1],// E       (4)
-            [1, -1, -1], // F       (5)
-            [1, -1, 1],  // G       (6)
-            [-1, -1, 1], // H       (7)
-            [-2, 0, 0],  // left    (8)
-            [2, 0, 0],   // right   (9)
-            [0, 2, 0],   // top     (10)
-            [0, -2, 0],  // bottom  (11)
-            [0, 0, 2],   // front   (12)
-            [0, 0, -2]   // back    (13)
+            [-1 * cos45 + -1 * sin45, 1, -1 * -sin45 + -1 * cos45], // A       (0)
+            [1 * cos45 + -1 * sin45, 1, 1 * -sin45 + -1 * cos45],   // B       (1)
+            [1 * cos45 + 1 * sin45, 1, 1 * -sin45 + 1 * cos45],     // C       (2)
+            [-1 * cos45 + 1 * sin45, 1, -1 * -sin45 + 1 * cos45],   // D       (3)
+            [-1 * cos45 + -1 * sin45, -1, -1 * -sin45 + -1 * cos45],// E       (4)
+            [1 * cos45 + -1 * sin45, -1, 1 * -sin45 + -1 * cos45],  // F       (5)
+            [1 * cos45 + 1 * sin45, -1, 1 * -sin45 + 1 * cos45],    // G       (6)
+            [-1 * cos45 + 1 * sin45, -1, -1 * -sin45 + 1 * cos45],  // H       (7)
+            [-2 * cos45 + 0 * sin45, 0, -2 * -sin45 + 0 * cos45],   // left    (8)
+            [2 * cos45 + 0 * sin45, 0, 2 * -sin45 + 0 * cos45],     // right   (9)
+            [0, 2, 0],                                              // top     (10)
+            [0, -2, 0],                                             // bottom  (11)
+            [0 * cos45 + 2 * sin45, 0, 0 * -sin45 + 2 * cos45],     // front   (12)
+            [0 * cos45 + -2 * sin45, 0, 0 * -sin45 + -2 * cos45]    // back    (13)
         ];
         
         const faces = [
@@ -222,10 +256,59 @@ class threeDScreen {
         }
     }
     
+    // drawRhombicDodecahedrons(sketch) {
+    //     const halfWidth = this.width / 2;
+    //     const halfHeight = this.height / 2;
+    //     const scaleFactor = 10;
+    //     const rhombicDodecahedronSize = this.tileSize;
+    
+    //     for (let i = 0; i < this.rhomdod.length; i++) {
+    //         const x = this.rhomdod[i].x * rhombicDodecahedronSize - halfWidth;
+    //         const y = this.rhomdod[i].y * rhombicDodecahedronSize - halfHeight;
+    //         const z = this.rhomdod[i].z * rhombicDodecahedronSize;
+    //         sketch.push();
+    //         sketch.translate(x, y, z);
+    //         sketch.scale(scaleFactor);
+    //         sketch.fill(this.rhomdod[i].color[0], this.rhomdod[i].color[1], this.rhomdod[i].color[2]);
+    //         this.drawRhombicDodecahedron(sketch, this.tileSize);
+    //         sketch.pop();
+    //     }
+    // }
+
+    drawPyramid(sketch, baseSize, height) {
+        // Base vertices
+        const v0 = [-baseSize / 2, baseSize / 2, 0];
+        const v1 = [baseSize / 2, baseSize / 2, 0];
+        const v2 = [baseSize / 2, -baseSize / 2, 0];
+        const v3 = [-baseSize / 2, -baseSize / 2, 0];
+        const apex = [0, 0, height];
+    
+        // Draw base
+        sketch.beginShape();
+        sketch.vertex(...v0);
+        sketch.vertex(...v1);
+        sketch.vertex(...v2);
+        sketch.vertex(...v3);
+        sketch.endShape(sketch.CLOSE);
+    
+        // Draw faces
+        const faces = [
+            [v0, v1, apex],
+            [v1, v2, apex],
+            [v2, v3, apex],
+            [v3, v0, apex]
+        ];
+    
+        faces.forEach(face => {
+            sketch.beginShape();
+            face.forEach(vertex => sketch.vertex(...vertex));
+            sketch.endShape(sketch.CLOSE);
+        });
+    }
+
     drawRhombicDodecahedrons(sketch) {
         const halfWidth = this.width / 2;
         const halfHeight = this.height / 2;
-        const sclaeFactor = 10;
     
         for (let i = 0; i < this.rhomdod.length; i++) {
             const x = this.rhomdod[i].x * this.tileSize - halfWidth;
@@ -233,9 +316,58 @@ class threeDScreen {
             const z = this.rhomdod[i].z * this.tileSize;
             sketch.push();
             sketch.translate(x, y, z);
-            sketch.scale(sclaeFactor);
-            sketch.fill(this.rhomdod[i].color[0], this.rhomdod[i].color[1], this.rhomdod[i].color[2]);
-            this.drawRhombicDodecahedron(sketch, this.tileSize);
+            if (highlight && this.rhomdod[i].z === highLayer) {
+                sketch.fill(0, 0, 255);
+            } else {
+                sketch.fill(this.rhomdod[i].color[0], this.rhomdod[i].color[1], this.rhomdod[i].color[2]);
+            }
+            sketch.box(this.tileSize);
+    
+            // Draw pyramids on each face of the cube
+            const pyramidHeight = this.tileSize / 2;
+            const halfTileSize = this.tileSize / 2;
+    
+            // Front face
+            sketch.push();
+            sketch.translate(0, 0, halfTileSize);
+            this.drawPyramid(sketch, this.tileSize, pyramidHeight);
+            sketch.pop();
+    
+            // Back face
+            sketch.push();
+            sketch.translate(0, 0, -halfTileSize);
+            sketch.rotateY(Math.PI);
+            this.drawPyramid(sketch, this.tileSize, pyramidHeight);
+            sketch.pop();
+    
+            // Left face
+            sketch.push();
+            sketch.translate(-halfTileSize, 0, 0);
+            sketch.rotateY(-Math.PI / 2);
+            this.drawPyramid(sketch, this.tileSize, pyramidHeight);
+            sketch.pop();
+    
+            // Right face
+            sketch.push();
+            sketch.translate(halfTileSize, 0, 0);
+            sketch.rotateY(Math.PI / 2);
+            this.drawPyramid(sketch, this.tileSize, pyramidHeight);
+            sketch.pop();
+    
+            // Top face
+            sketch.push();
+            sketch.translate(0, halfTileSize, 0);
+            sketch.rotateX(-Math.PI / 2);
+            this.drawPyramid(sketch, this.tileSize, pyramidHeight);
+            sketch.pop();
+    
+            // Bottom face
+            sketch.push();
+            sketch.translate(0, -halfTileSize, 0);
+            sketch.rotateX(Math.PI / 2);
+            this.drawPyramid(sketch, this.tileSize, pyramidHeight);
+            sketch.pop();
+    
             sketch.pop();
         }
     }
@@ -250,6 +382,7 @@ class threeDScreen {
                 this.drawHexagons(sketch);
                 break;
             case "rhombicDodecahedron":
+                //this.drawPyramid(sketch, this.tileSize, this.tileSize);
                 this.drawRhombicDodecahedrons(sketch);
                 break;
             default:
