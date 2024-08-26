254a9515392dc090ec55046fe4bcc32a8532d6d4
Kestrel280 <downey.samp@gmail.com>
2024-08-25 22:31:55 -0400

    (WebVis) Adds a couple of 'example' buttons

--- WebVis/src/GUI.js
+++ WebVis/src/GUI.js
@@ -1,4 +1,5 @@
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
+import { Scenario } from './Scenario.js';
 
 const SliderType = Object.freeze({
     LINEAR: 0,
@@ -29,10 +30,23 @@ class GuiGlobalsHelper {
 
 export const gGui = new GUI();
 
+window._loadExampleScenario1 = async () => {
+    const scen = await fetch('../Scenarios/3d2rMeta.scen').then(response => response.text());
+    new Scenario(scen);
+}
+window._loadExampleScenario2 = async () => {
+    const scen = await fetch('../Scenarios/820.scen').then(response => response.text());
+    new Scenario(scen);
+}
 document.addEventListener("DOMContentLoaded", function () {
     gGui.add(new GuiGlobalsHelper('gwAnimSpeed', 1.0, SliderType.QUADRATIC), 'value', 0.0, 5.0, 0.1).name("Anim Speed");
     gGui.add(new GuiGlobalsHelper('gwAutoAnimate', false), 'value').name("Auto Animate");
     gGui.add(window.gwUser, 'toggleCameraStyle').name("Toggle Camera Style");
     gGui.add(window, '_requestForwardAnim').name("Step Forward");
     gGui.add(window, '_requestBackwardAnim').name("Step Backward");
+
+    // TODO: Auto-populate this folder based on scenarios present in ../Scenarios
+    const _folder = gGui.addFolder("Example Scenarios");
+    _folder.add(window, '_loadExampleScenario1').name("2x2x2 Metamodule");
+    _folder.add(window, '_loadExampleScenario2').name("820");
 });
