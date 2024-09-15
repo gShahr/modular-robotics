e3d58840fd85cbb82064324c52d64fb5558e1ade
Kestrel280 <downey.samp@gmail.com>
2024-09-15 18:45:29 -0400

    (WebVis) PARTIAL PROGRESS -- Dramatically simplifies pivot logic

--- WebVis/src/Move.js
+++ WebVis/src/Move.js
@@ -25,19 +25,12 @@ export class Move{
         //      Simply take the cross-product of the face-normal and the delta-position.
         //  To make this rotation happen about a specific point,
         //      we need to translate the shape such that the point lies at the origin.
-        //  For rotation about an EDGE, we select the midpoint of the edge.
-        //  A shape's midsphere is a sphere which is tangent to every edge of the shape.
-        //      For a regular polyhedron, that tangent point happens to lie on the midpoint of each edge.
-        //      This makes the LENGTH of our translation trivial -- it's just the radius of the midsphere.
-        //  The translation DIRECTION is much trickier.
+        //      (For rotation about an EDGE, we select the midpoint of the edge.)
+        //  The translation DIRECTION is trickier.
         //  We need to point a vector from the origin of the shape to the corresponding edge.
         //  For that, we need to figure out which edge we're pivoting about.
         //  However, all we have is the face-normal ("anchor direction"), and the delta-position.
-        //  Somewhere, we need to reconstruct the translation direction from that information.
         //
-        //  (I derived this by dumb luck, by staring at cases until patterns emerged; 
-        //   I don't know what properties allow the following to be true. So it's magic, for now.)
-        //  
         //  The coordinate system used has its "absolute-origin" at the center of the shape that we are pivoting around.
         //  That is, the origin does NOT lie in the pivoting shape; it's in the "anchor" shape!
         //
@@ -45,20 +38,7 @@ export class Move{
         //  Consider if this traversal was a linear slide from the start to the end:
         //      then, there is an "average position" of this linear translation,
         //      and we can draw a vector from the absolute-origin to this "average position".
-        //      (We don't normalize this vector -- not yet!)
-        //
-        //  But we're pivoting: the origin of the shape actually follows an arc, not a line segment.
-        //      We need to correct for this arc. It so happens that, for RD's, all valid moves
-        //      have delta positions which result in a movement of +/- 1 on exactly 2 axis.
-        //      (E.g. <0, 0, 1>, <0, -1, 0>, etc. are not valid delta positions;
-        //       but <-1, 0, 1>, <1, 1, 0>, <0, -1, -1> etc. are)
-        //
-        //  One axis is always "untouched". This axis is where we need to add the correction factor.
-        //      The magnitude of the correction factor is 0.5: we need to SHRINK the length of the
-        //      corresponding element in the linear-translation vector by 0.5.
-        //
-        //  Applying this correction factor yields a vector in the direction of the midpoint of the correct edge.
-        //  So we can simply scale it to the correct length (the midsphere radius) and use it as our translation vector.
+        //  This "average position" lies at the midpoint of the edge about which we're pivoting.
 
         switch(moveType) {
             case MoveType.PIVOT: {
@@ -70,25 +50,13 @@ export class Move{
                 //  (This happens to be neatly encoded in anchorDir;
                 //   we just need to re-scale it to appropriate length)
                 let _startPos = this.anchorDir.clone().multiplyScalar(this.inscsphere * 2);
-                let _endPos = _startPos.clone().add(this.deltaPos);
 
                 // Determine the midpoint of the linear-translation
-                let _linearTranslation = _startPos.clone().multiplyScalar(2).add(this.deltaPos);
+                let _linearTranslation = _startPos.clone().add(this.deltaPos);
 
-                // Calculate our initial pass at the arc-correction factor:
-                //  Extract the 0-element of the delta-position
-                let _arcTranslation = new THREE.Vector3(1.0, 1.0, 1.0)
-                    .sub(this.deltaPos.clone().abs());
+                let _translationDir = _linearTranslation.clone().normalize();
 
-                // Need to determine what sign this arc-correction factor should have
-                //  (opposite the corresponding element in the linear-translation vector)
-                let _arcSign = Math.sign(
-                    new THREE.Vector3()
-                    .multiplyVectors(_arcTranslation, _linearTranslation) // element-wise multiplication
-                    .negate().sum());
-
-                // Apply our arc correction factor to the linear translation and normalize
-                let _translationDir = _linearTranslation.clone().add(_arcTranslation.multiplyScalar(_arcSign)).normalize();
+                console.log({deltaPos, _startPos, _linearTranslation, _translationDir});
 
                 // Scale to midsphere length
                 this.postTrans = _translationDir.clone().multiplyScalar(this.midsphere);
@@ -108,8 +76,6 @@ export class Move{
 
     reverse() {
         let newDeltaPos = this.deltaPos.clone().negate();
-        let testVec = new THREE.Vector3(1.0, 1.0, 1.0);
-        let manhattanDistance = newDeltaPos.abs().clone().dot(testVec);
 
         // In the coordinate system centered at the origin of the "anchor" shape,
         //  take our position and subtract the delta-position of the move.
