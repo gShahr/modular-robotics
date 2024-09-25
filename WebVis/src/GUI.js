288fcacd774bd945d72d66f4bbc9436b5f5ab04a
Kestrel280 <downey.samp@gmail.com>
2024-09-24 21:29:25 -0400

    (WebVis) Reverts previous; moving to a semi-automatic approach to populating example scenarios...

--- WebVis/src/GUI.js
+++ WebVis/src/GUI.js
@@ -3,6 +3,15 @@ import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 import { Scenario } from './Scenario.js';
 import { gScene, gLights } from './main.js';
 
+// Exact filenames of example scenarios in /Scenarios/
+let EXAMPLE_SCENARIOS = [
+    '2x2x2 Metamodule',
+    '3x3 Metamodule',
+    'Cube Debugging',
+    'RD Debugging',
+    'Slide Debugging'
+]
+
 const SliderType = Object.freeze({
     LINEAR: 0,
     QUADRATIC: 1
@@ -62,18 +71,27 @@ document.addEventListener("DOMContentLoaded", async function () {
     gGui.add(window, '_requestBackwardAnim').name("Step Backward");
 
     const _folder = gGui.addFolder("Example Scenarios");
-    await fetch("./Scenarios/").then(async response => {
-        let scen, item, name, filepath;
-        let raw = await response.text();
-        let el = document.createElement('html');
-        el.innerHTML = raw;
-        let list = el.getElementsByClassName("file scen");
-        for (item in list) {
-            name = list[item].title;
-            if (!name) { continue; }
-            name = name.split('.')[0];
-            _exampleLoaders[name] = _generateExampleLoader(name);
-            _folder.add(_exampleLoaders, name).name(name);
-        }
-    });
+    for (let i in EXAMPLE_SCENARIOS) {
+        let ex = EXAMPLE_SCENARIOS[i];
+        _exampleLoaders[ex] = _generateExampleLoader(ex);
+        _folder.add(_exampleLoaders, ex).name(ex);
+    }
+
+    // Following code only works with certain server backends (that allow fetching directories).
+    //  Purpose was to automatically identify all elements in the /Scenarios/ subfolder.
+    //
+    // await fetch("./Scenarios/").then(async response => {
+    //     let scen, item, name, filepath;
+    //     let raw = await response.text();
+    //     let el = document.createElement('html');
+    //     el.innerHTML = raw;
+    //     let list = el.getElementsByClassName("file scen");
+    //     for (item in list) {
+    //         name = list[item].title;
+    //         if (!name) { continue; }
+    //         name = name.split('.')[0];
+    //         _exampleLoaders[name] = _generateExampleLoader(name);
+    //         _folder.add(_exampleLoaders, name).name(name);
+    //     }
+    // });
 });
